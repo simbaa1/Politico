43b384c1b9d1968c91e952ffdacc30a2be5c4c83
Chris Simba <simbachris08@gmail.com>
2019-02-07 09:20:09 +0300

    [feature #163711932] Set up blueprints

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -1,9 +1,11 @@
 import os
 from flask import Flask
+from .v1.views.views import bp_1
 
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
-
-
+    app.register_blueprint(bp_1)
+    
+    
     return app
